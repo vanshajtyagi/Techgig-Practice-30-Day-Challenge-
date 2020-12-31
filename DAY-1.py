c7565e1f07a7ee9afdbb5b6137b3c91e6eeb7836
Vanshaj Tyagi <45164555+vanshajtyagi@users.noreply.github.com>
2020-12-31 21:53:06 +0530

    Day 1

---
+++ DAY-1.py
@@ -0,0 +1,9 @@
+''' Read input from STDIN. Print your output to STDOUT '''
+    #Use input() to read input from STDIN and use print to write your output to STDOUT
+
+def main():
+    n=input()
+    print("Hello Techgig\n"+n)
+
+main()
+
