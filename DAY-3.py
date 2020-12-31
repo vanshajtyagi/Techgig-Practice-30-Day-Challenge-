1988ba5f9abefabee9eac3e6838d677d75858576
Vanshaj Tyagi <45164555+vanshajtyagi@users.noreply.github.com>
2020-12-31 21:59:09 +0530

    Day 3

---
+++ DAY-3.py
@@ -0,0 +1,11 @@
+''' Read input from STDIN. Print your output to STDOUT '''
+    #Use input() to read input from STDIN and use print to write your output to STDOUT
+
+def main():
+    a = float(input()) 
+    b = int(input()) 
+    c = int(input()) 
+    s = (abc)/100 
+    print(int(abs(s)))
+
+main()
