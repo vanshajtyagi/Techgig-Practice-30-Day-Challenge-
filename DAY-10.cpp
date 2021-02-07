bfc36684fe7812671353b5425c2d8599114ec134
Vanshaj Tyagi <45164555+vanshajtyagi@users.noreply.github.com>
2021-02-07 15:46:02 +0530

    Create DAY-10.cpp

---
+++ DAY-10.cpp
@@ -0,0 +1,25 @@
+/* Read input from STDIN. Print your output to STDOUT*/
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <limits.h>
+#include <stdbool.h>
+#include<bits/stdc++.h>
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;cin>>n;
+    int a[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>a[i];
+    }
+    int d= sizeof(a)/sizeof(a[0]);
+    sort(a,a+d);
+    cout<<a[n-2];
+
+
+}
